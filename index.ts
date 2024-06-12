c4d70ffe0e609458fa37c6d6ccf4f098b61530e7
ngdangkhanhdev <ngdangkhanh233@gmail.com>
2024-06-12 15:27:41 +0700

    Config pug + public static file

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("OK");
+import express, { Express, Request, Response } from "express";
+//const express = require("express");
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+
+app.get("/topics", (req: Request, res: Response) => {
+    res.render("client/pages/topics/index");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
